07e17e460de1f1106040fe9742ca4e9d4499948f
Joseph Shieh <joseph.shieh@gmail.com>
2014-02-10 15:28:47 -0800

    Finished part one
    Can now redirect requests to web servers and receive responses and
    display web page

--- proj2/HttpProxy.java
+++ proj2/HttpProxy.java
@@ -1,13 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 
 /**
  * Joseph Shieh, 1031718, josephs2@uw.edu
@@ -44,9 +43,8 @@ public class HttpProxy {
 				Socket client = server.accept();
 				// Now that we have a client to to communicate with, create new thread
 				Listen l = new Listen(client);
-				Thread t1 = new Thread(l);
-				t1.start();
-
+				Thread t = new Thread(l);
+				t.start();
 			} catch (IOException e) {
 				System.out.println("Failed to accept connection.");
 			}
@@ -58,7 +56,6 @@ public class HttpProxy {
 		System.out.println("Usage: java HttpProxy <service port>");
 		System.exit(1);
 	}
-
 }
 
 class Listen implements Runnable {
@@ -70,21 +67,21 @@ class Listen implements Runnable {
 
 	@Override
 	public void run() {
-		try {
-			PrintWriter outputStream = new PrintWriter(socket.getOutputStream(), true);
+		try{
+			OutputStream outputStream = socket.getOutputStream();
 			BufferedReader inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			String inputLine, outputLine = "";
 			boolean first = true;
 			String temp, hostAddr = "";
 			int port = 80;
+			String eof = "\r\n"; // CR LF String
 			while ((inputLine = inputStream.readLine()) != null) {
 				if (first){
 					// Print the first line of the request
 					System.out.println(inputLine);
 					first = false;
 				}
-				// TODO: Look for CR-LF to terminate loop
-				System.out.println(inputLine);
+				//System.out.println(inputLine);
 				temp = inputLine.toLowerCase();
 				if (temp.startsWith("host:")) {
 					String host = inputLine.split(": ")[1];
@@ -93,81 +90,67 @@ class Listen implements Runnable {
 						port = Integer.parseInt(hostString[1]);
 					}
 					hostAddr = hostString[0];
-					outputLine += inputLine;
-
-					//System.out.println("young fuck: " + host);
+					outputLine += inputLine + eof;
 				} else if (temp.startsWith("connection:")) {
-					String connection = inputLine;
-					outputLine += "Connection: close";
-					//System.out.println("young con: " + temp);
+					outputLine += "Connection: close" + eof;
 				} else if (temp.equals("")) {
-					System.out.println("Last Line!");
-					outputLine += inputLine;
-					SendAndReceive s = new SendAndReceive(outputLine, hostAddr, port);
-					Thread t1 = new Thread(s);
-					t1.start();
+					//System.out.println("Last Line!");
+					outputLine += eof;
+					//System.out.println(hostAddr);
+					//System.out.println(port);
+					InetAddress destAddr = InetAddress.getByName(hostAddr);
+					SendAndReceive s = new SendAndReceive(destAddr, port, outputLine, outputStream);
+					Thread t = new Thread(s);
+					t.start();
 				} else { // not host or connection
-					outputLine += inputLine;
+					outputLine += inputLine + eof;
 				}
-				outputLine += (char) 13 + ""; // CR string
-				outputLine += (char) 10 + ""; // LF string
-				//System.out.println("Output so far:");
-				//System.out.println(outputLine);
-				// break;
 				// manually close connection or wait for timeout
 			}
-
 		} catch (IOException e) {
-
+			System.out.println("Failed to get socket.");
+			System.exit(1);
 		}
 	}
 
 }
 
 class SendAndReceive implements Runnable {
-	DatagramSocket sendAndReceive;
+	Socket sendAndReceive;
 	String outputLine;
-	String hostAddr;
-	int port;
+	OutputStream clientOutputStream;
 
-	public SendAndReceive(String outputLine, String hostAddr, int port)  {
+	public SendAndReceive(InetAddress destAddr, int port, String outputLine, OutputStream clientOutputStream) {
 		try {
-			sendAndReceive = new DatagramSocket();
-		} catch (SocketException e) {
-			System.out.println("Failed to create send and receive socket");
-			System.exit(1);
+			sendAndReceive = new Socket(destAddr, port);
+		} catch (IOException e) {
+			System.out.println("Failed to find socket on web server.");
 		}
 		this.outputLine = outputLine;
-		this.hostAddr = hostAddr;
-		this.port = port;
+		this.clientOutputStream = clientOutputStream;
 	}
 
 	@Override
 	public void run() {
-
 		try {
 			// Redirect the packet to the web server
-			System.out.println("Sending to ... hostaddr: " + hostAddr);
-			System.out.println(outputLine);
-			System.out.println("End of request");
-			InetAddress destAddr = InetAddress.getByName(hostAddr);
-			DatagramPacket packet = new DatagramPacket(outputLine.getBytes(), outputLine.getBytes().length, destAddr, port);
-			System.out.println("a");
-			sendAndReceive.send(packet);
-			System.out.println("b");
-			// Receive response from the web server
+			//System.out.println(outputLine);
+			InputStream inputStream = sendAndReceive.getInputStream();
+			PrintWriter outputStream = new PrintWriter(sendAndReceive.getOutputStream(), true);
+			// Write to web server as string
+			outputStream.write(outputLine);
+			outputStream.flush();
+			// Read response as bytes
 			byte[] buffer = new byte[1000];
-			DatagramPacket response = new DatagramPacket(buffer, buffer.length);
-			sendAndReceive.receive(response);
-			System.out.println("c");
-			String responseString = new String(response.getData());
-			System.out.println("Response string: " + responseString);
-			//outputStream.print();
+			int len;
+			while((len = inputStream.read(buffer)) > 0) {
+				// Write to client as bytes as well
+				this.clientOutputStream.write(buffer, 0, len);
+			}
+			this.clientOutputStream.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-
 	}
 
 }
